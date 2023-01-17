56dca158fb9aac0d18b51fd9163447c2140d9517
NemethBalint96 <nemeth.balint1996@gmail.com>
2023-01-17 11:36:54 +0100

    Animator and Weapon Swing Animation

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -12,6 +12,7 @@ public class Weapon : Collidable
     private SpriteRenderer spriteRenderer;
 
     // Swing
+    private Animator anim;
     private float cooldown = 0.5f;
     private float lastSwing;
 
@@ -19,6 +20,7 @@ public class Weapon : Collidable
     {
         base.Start();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     protected override void Update()
@@ -56,6 +58,6 @@ public class Weapon : Collidable
 
     private void Swing()
     {
-        Debug.Log("Swing");
+        anim.SetTrigger("Swing");
     }
 }
