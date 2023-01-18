5490ee1551619b98b5eb50ea8020edc50bfece6c
NemethBalint96 <nemeth.balint1996@gmail.com>
2023-01-18 11:10:39 +0100

    Character Menu and UI System

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Chest : Collectable
@@ -13,6 +11,7 @@ public class Chest : Collectable
         {
             collected = true;
             GetComponent<SpriteRenderer>().sprite = emptyChest;
+            GameManager.instance.pesos += pesosAmount;
             GameManager.instance.ShowText("+" + pesosAmount + " pesos!", 25, Color.yellow, transform.position, Vector3.up * 25, 1.5f);
         }
         
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,7 +27,7 @@ public class GameManager : MonoBehaviour
 
     // References
     public Player player;
-    // public weapon weapon...
+    public Weapon weapon;
     public FloatingTextManager floatingTextManager;
 
     // Logic
@@ -38,6 +38,68 @@ public class GameManager : MonoBehaviour
     public void ShowText(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
     {
         floatingTextManager.Show(msg, fontSize, color, position, motion, duration);
+    }
+
+    // Upgrade Weapon
+    public bool TryUpgradeWeapon()
+    {
+        // is the weapon max level?
+        if (weaponPrices.Count <= weapon.weaponLevel)
+            return false;
+
+        if (pesos >= weaponPrices[weapon.weaponLevel])
+        {
+            pesos -= weaponPrices[weapon.weaponLevel];
+            weapon.UpgradeWeapon();
+            return true;
+        }
+
+        return false;
+    }
+
+    // Experience System
+    public int GetCurrentLevel()
+    {
+        int r = 0;
+        int add = 0;
+
+        while(experience >= add)
+        {
+            add += xpTable[r];
+            r++;
+
+            if (r == xpTable.Count) // Max Level
+                return r;
+        }
+
+        return r;
+    }
+
+    public int GetXpToLevel(int level)
+    {
+        int r = 0;
+        int xp = 0;
+
+        while (r < level)
+        {
+            xp += xpTable[r];
+            r++;
+        }
+
+        return xp;
+    }
+
+    public void GrantXp(int xp)
+    {
+        int currLevel = GetCurrentLevel();
+        experience += xp;
+        if (currLevel < GetCurrentLevel())
+            OnLevelUp();
+    }
+
+    public void OnLevelUp()
+    {
+        player.OnLevelUp();
     }
 
     // Save state
@@ -54,7 +116,7 @@ public class GameManager : MonoBehaviour
         s += "0" + "|";
         s += pesos.ToString() + "|";
         s += experience.ToString() + "|";
-        s += "0";
+        s += weapon.weaponLevel.ToString();
 
         PlayerPrefs.SetString("SaveState", s);
     }
@@ -68,8 +130,15 @@ public class GameManager : MonoBehaviour
 
         // Change player skin
         pesos = int.Parse(data[1]);
+
+        // Experience
         experience = int.Parse(data[2]);
+        if (GetCurrentLevel() != 1)
+            player.SetLevel(GetCurrentLevel());
+
         // Change the weapon level
+        weapon.SetWeaponLevel(int.Parse(data[3]));
+
 
         Debug.Log("LoadState");
     }
